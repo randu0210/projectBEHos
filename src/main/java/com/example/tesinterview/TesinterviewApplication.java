c4a2e88c852d84dd792195dbf11ddf7ede9e7d54
ran0210 <58762707+randu0210@users.noreply.github.com>
2025-02-27 01:59:32 +0700

    push first init project

---
+++ src/main/java/com/example/tesinterview/TesinterviewApplication.java
@@ -0,0 +1,13 @@
+package com.example.tesinterview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesinterviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesinterviewApplication.class, args);
+	}
+
+}
